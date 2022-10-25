f11b356455a0c110060c616bf0f84d4232605446
tethys <tethyslt@gmail.com>
2022-10-25 02:23:09 -0400

    updated

--- teste.py
+++ teste.py
@@ -1,39 +1,25 @@
+from utils import *
+from scipy.optimize import curve_fit
 
-with open("gamv.par", "w") as f:
-    f.write("                         Parameters for GAMV                                 \n")
-    f.write("                         *******************                                 \n")
-    f.write("START OF PARAMETERS:                                                         \n")
-    f.write("                                                                             \n")
-    f.write("START OF MAIN:                                                               \n")
-    f.write("0.0  0.0  0.0  -directions: ang1,2,3 (GSLIB definition)                      \n")
-    f.write("1  -number of variables                                                      \n")
-    f.write("2  -number of nested structures                                              \n")
-    f.write("0  -inverse distance weighting (0=no, 1=yes)                                 \n")
-    f.write("0  -number of pairs weighting (0=no, 1=yes)                                  \n")
-    f.write("10  -minimum number of pairs to use                                          \n")
-    f.write("5000  -number of iterations                                                  \n")
-    f.write("varfit.ps  -file for PostScript output                                       \n")
-    f.write("varfit.var  -file for variogram model(s)                                     \n")
-    f.write("                                                                             \n")
-    f.write("START OF VARIOGRAMS SPECIFICATION:                                           \n")
-    f.write("1 1 1 1 -ivar1, ivar2, idir, ivarn                                           \n")
-    f.write("gamv_out.out  - variogram file                                               \n")
-    f.write("Fe Semivariogram   - variogram title                                         \n")
-    f.write("                                                                             \n")
-    f.write("START OF PAGE TITLES:                                                        \n")
-    f.write("1 1  -ivar1, ivar2                                                           \n")
-    f.write("Fe Variogram  -title                                                         \n")
-    f.write("                                                                             \n")
-    f.write("START OF ADVANCED OPTIONS:                                                   \n")
-    f.write("0 0 0  -zonal Anis: Hmax, Hmin, Vert (0=no, 1=yes)                           \n")
-    f.write("0 0 0   -cyclicity: Hmax, Hmin, Vert (0=no, 1=yes)                           \n")
-    f.write("0     -number of sills to fix                                                \n")
-    f.write("0     -number of nugget effects to fix                                       \n")
-    f.write("0     -number of structure types to fix                                      \n")
-    f.write("0     -number of Hmax ranges to fix                                          \n")
-    f.write("0  -number of Hmin ranges to fix                                             \n")
-    f.write("0  -number of Vert ranges to fix                                             \n")
-    f.write("0  -number of variogram preferences                                          \n")
-    f.write("0 10.0 -fix Hmax/Vert anis. (0=no, 1=yes)                                    \n")
-    f.write("0 1.0 -fix Hmin/Hmax anis. (0=no, 1=yes)                                     \n")
-    f.write("                                                                             \n")
+
+def exponential_two(h, r1, r2, c):
+    result = np.zeros(h.shape)
+    for idx, r in enumerate(h):
+        if r <= r1:
+            result[idx] = c * (1 - math.exp(-3*r/r1))
+        elif r1 <= r <= r2:
+            result[idx] = c + (1 - c) * (1 - math.exp(-3*(r-r1) / r2))
+        else:
+            result[idx] = 1
+    return result
+
+
+def vmodel(variogram, guess=[10, 110, 0.85]):
+    x = variogram[1:, 0]
+    parameters = np.zeros(variogram.shape[1], 3)
+    for i in range(2, variogram.shape[1]):
+        y = variogram[1:, i]
+        parameters[i-2], _ = curve_fit(exponential_two, x, y, p0=guess)
+    return parameters
+
+pass
