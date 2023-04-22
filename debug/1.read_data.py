47a2d17aa58b25a16f27de74de8623602992c341
tethys <tethyslt@gmail.com>
2023-04-21 23:51:44 -0400

    updated

--- debug/1.read_data.py
+++ debug/1.read_data.py
@@ -2,18 +2,18 @@ import os
 from utils import *
 
 'read origin data'
-rawdata, show, y_label = read_data(test=False)
+rawdata, show, y_label = read_data(test=True)
 
 'calculate variogram for rawdata'
 if os.path.exists('./result/variogram_exhausted.npy'):
-    rawdata_variogram = np.load('./variogram_exhausted.npy')
+    rawdata_variogram = np.load('./variogram_exhausted_test.npy')
 else:
     pt = preprocessing.PowerTransformer(method = 'box-cox')
     data = rawdata.copy()
     data[:, 2:] = pt.fit_transform(rawdata[:, 2:])
     rawdata_variogram = variogram_gam(data, lag=4, nlag=50, trace=True)
-    np.save('./result/variogram_exhausted.npy', rawdata_variogram)
+    np.save('./result/variogram_exhausted_test.npy', rawdata_variogram)
 model = vmodel(rawdata_variogram, guess=[0, 10, 110, 0.85])
 plot_variogram([rawdata_variogram], ele=len(y_label), y_label=y_label, line_label=['Exhausted data'], colors=['b'],
-               alphas=[1], title='variogram of exhausted data', vmodel=None)
-np.save('./result/rawdata.npy', rawdata)
+               alphas=[1], title='variogram of exhausted data_test', vmodel=None)
+np.save('./result/rawdata_test.npy', rawdata)
--- debug/2.MF.py
+++ debug/2.MF.py
@@ -1,4 +1,3 @@
-import os
 from utils import *
 
 rawdata = np.load('./result/rawdata.npy')
@@ -7,6 +6,8 @@ label = ['Ag', 'Al', 'Au', 'B', 'Ba', 'Be', 'Bi', 'Ca', 'Co', 'F', 'Fe', 'K',
          'La', 'Li', 'Mg', 'Mn', 'Mo', 'Nb', 'P', 'Sn', 'Sr', 'Ti', 'V', 'Y1', 'Zr']
 n_lm = int(335*335*0.1)
 lm_loc = np.random.randint(0, 335*335, size=n_lm)
+mf_repeat = 50
+nlag = 50
 
 "random landmarks"
 landmarks = rawdata[lm_loc, :]
@@ -17,16 +18,16 @@ landmarks_cdf = convert_to_cdf(landmarks.copy(), show_config=show_config, if_sho
 'Generating MFs'
 mf_raw_container = np.zeros((landmarks.shape[0], landmarks.shape[1], mf_repeat))
 mf_cdf_container = mf_raw_container.copy()
-mf_variogram_container = np.zeros((nlag + 2, sum(range(1, data.shape[1] - 1)) + 2, mf_repeat))
+mf_variogram_container = np.zeros((nlag + 2, sum(range(1, rawdata.shape[1] - 1)) + 2, mf_repeat))
 if_show = True
 for r in tqdm(range(mf_repeat), position=0, leave=True):
-    mf_raw, mf_cdf = transport(landmarks_cdf.copy(), if_show=if_show, show_config=show)
+    mf_raw, mf_cdf = transport(landmarks_cdf.copy(), if_show=if_show, show_config=show_config)
     mf_raw_container[:, :, r] = mf_raw.copy()
     mf_cdf_container[:, :, r] = mf_cdf.copy()
     if_show = False
 mf_ave = np.sum(mf_raw_container, axis=2) / mf_repeat
-plt.scatter(landmarks_cdf[0, show[0]], landmarks_cdf[0, show[1]], marker='+', c='b', label='landmark point')
-plt.scatter(mf_cdf_container[0, show[0], :], mf_cdf_container[0, show[1], :], marker='x', c='r',
+plt.scatter(landmarks_cdf[0, show_config[0]], landmarks_cdf[0, show_config[1]], marker='+', c='b', label='landmark point')
+plt.scatter(mf_cdf_container[0, show_config[0], :], mf_cdf_container[0, show_config[1], :], marker='x', c='r',
             label='morphing factors')
 plt.legend()
 plt.title('Pairing of landmark point and morphing factors')
